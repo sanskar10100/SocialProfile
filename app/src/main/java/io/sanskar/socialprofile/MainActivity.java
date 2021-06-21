164c107fed4577cb2246aefc17a202a39fc7014e
Sanskar Agrawal <sanskar10100@gmail.com>
2021-06-21 15:58:15 +0530

    Complete changelog:
    - Added logo and progress bar in activity_main.xml
    - Polished activity_profile_details.xml
    - Added round imageview
    - Implemented instant exit on login failure
    - Added null checks for name, email and phone
    - Updated launcher icon

--- app/src/main/java/io/sanskar/socialprofile/MainActivity.java
+++ app/src/main/java/io/sanskar/socialprofile/MainActivity.java
@@ -19,6 +19,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 292;
+    private static final int PROFILE_DETAILS = 519;
     private static final String TAG = "MainActivity";
 
     @Override
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         Intent signInIntent = AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
+                .setLogo(R.drawable.social_profile_logo)
                 .build();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
@@ -44,12 +46,16 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == RC_SIGN_IN) {
             if (resultCode == RESULT_OK) {
                 Log.d(TAG, "onActivityResult: Login Okay");
-                Toast.makeText(this, "You're signed in as " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(this, ProfileDetails.class));
+                startActivityForResult(new Intent(this, ProfileDetails.class), PROFILE_DETAILS);
             } else {
                 Log.d(TAG, "onActivityResult: Login Failed!");
                 Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                finish();
             }
+        }
+
+        if (requestCode == PROFILE_DETAILS) {
+            finish();
         }
     }
 }
--- app/src/main/java/io/sanskar/socialprofile/ProfileDetails.java
+++ app/src/main/java/io/sanskar/socialprofile/ProfileDetails.java
@@ -3,16 +3,52 @@ package io.sanskar.socialprofile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import io.sanskar.socialprofile.databinding.ActivityProfileDetailsBinding;
 
 public class ProfileDetails extends AppCompatActivity {
+
+    ActivityProfileDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_details);
+        binding = ActivityProfileDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        Glide.with(this).load(currentUser.getPhotoUrl()).into(binding.imageUserPhoto);
 
+        String displayName = currentUser.getDisplayName();
+        if (displayName == null) {
+            binding.textViewUsername.setText("Not Available");
+        } else {
+            binding.textViewUsername.setText(displayName);
+        }
+
+        String email = currentUser.getEmail();
+        if (email == null) {
+            binding.textViewUserMail.setText("Not Available");
+        } else {
+            binding.textViewUserMail.setText(email);
+        }
+
+        String phoneNumber = currentUser.getPhoneNumber();
+        if (phoneNumber == null) {
+            binding.textViewUserPhone.setText("Not Available");
+        } else {
+            binding.textViewUserPhone.setText(phoneNumber);
+        }
+    }
+
+    public void onSignOutButtonClicked(View view) {
+        AuthUI.getInstance().signOut(this);
+        finish();
     }
 }
