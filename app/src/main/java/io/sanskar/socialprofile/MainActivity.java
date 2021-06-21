ce65de0c5ca67db614abde3ac6e5f9f214ebe873
Sanskar Agrawal <sanskar10100@gmail.com>
2021-06-21 13:36:56 +0530

    Added login with Google

--- app/src/main/java/io/sanskar/socialprofile/MainActivity.java
+++ app/src/main/java/io/sanskar/socialprofile/MainActivity.java
@@ -1,14 +1,49 @@
 package io.sanskar.socialprofile;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 292;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build());
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Log.d(TAG, "onActivityResult: Login Okay");
+            } else {
+                Log.d(TAG, "onActivityResult: Login Failed!");
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
